4f742e64dbde12aab1ee57495af4e3ddaa50c79f
LoranSwega <roneo9009@hotmail.fr>
2016-03-31 02:28:19 +0200

    Commit 1
    Intégration du webdesign, et début du traitement des données en php

--- Code/index.php
+++ Code/index.php
@@ -13,12 +13,82 @@ $posts = get_posts();
 
 	<meta name="description" content="Ceci est un test de développement" />
 
+	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="css/style.css">
+	
 
 </head>
 <body>
-	
-	<!-- CONTENU DU SITE -->
+	<header>
+		<div class="banner">
+			<img src="img/banner.png" />
+		</div>
+		<div class="logo">
+			<img src="img/logo.png" />
+		</div>
+	</header>
+	<main>
+		<h1>Office de Tourisme de Martigues-sur-mer</h1>
+		<section>
+			<h2>Les bons plans du territoire</h2>
+<?php
+			for($i=0;$i<=count($posts)-1;$i++)
+			{
+				$username=$posts[$i]->username;
+				$avatar=$posts[$i]->profile_pic;
+				$premium=$posts[$i]->is_premium;
+				$date=$posts[$i]->date;
+				$type=$posts[$i]->type;
+				$content=$posts[$i]->post_content;
+				$url=$posts[$i]->url;
+				if ($type=="news")
+				{
+					//color rond = bleu
+				}
+				else
+				{
+					//color rond = orange
+				}
+				if ($premium==true)
+				{
+					//color du nom rond = orange
+				}
+				else
+				{
+					//color du nom = noir
+				}
+				echo "<article><div>";
+				echo "<div class='avatar'><img src='img/profile_pics/".$avatar."' /></div>";
+				echo "<div><h3class='titreAnnonce'>".$username."</h3>".date('d/m/Y', $date)."</div>";
+				echo "</div></article>";
+				
+				
+				
+			}
+			//echo var_dump($posts);
+?>
+			<article>
+				<div>
+					<div class='avatar'>
+						
+					</div>
+					<div class="titreAnnonce">
+						<h3>Restaurant du golf</h3>
+					</div>
+					<div>
+						icone
+					</div>
+				</div>
+			</article>
+		
+		</section>
+		
+	</main>
+	<footer>
+		<p>
+			Fourni par Bouches-du-Rhône Tourisme. <a href="#">Visitez la Provence.</a>
+		</p>
+	</footer>
 
 </body>
 </html>
